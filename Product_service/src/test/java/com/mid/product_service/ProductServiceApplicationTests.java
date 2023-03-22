f32e668a455f5479af71890d9f3caaeb0550a48a
Hamza_ELHAOU <hamzaelhaou.2000@gmail.com>
2023-03-22 14:57:52 +0000

    initail commit Hamza

--- Product_service/src/test/java/com/mid/product_service/ProductServiceApplicationTests.java
+++ Product_service/src/test/java/com/mid/product_service/ProductServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.sid.inventoryservice;
+package com.mid.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InventoryServiceApplicationTests {
+class ProductServiceApplicationTests {
 
     @Test
     void contextLoads() {
